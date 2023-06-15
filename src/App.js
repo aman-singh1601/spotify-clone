0d1dff47a7aa2a307648ff097707b8a86d0f0b4b
Aman <aman16singh2001@gmail.com>
2023-06-15 20:51:17 +0530

    got the account details from spotify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login/Login';
+import { getTokenFromUrl } from './Login/spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+
+
+const spotify=new SpotifyWebApi();
 
 function App() {
+
+  const [token,setToken]=useState(null);
+
+  useEffect(()=>{
+    const hash=getTokenFromUrl();
+    window.location.hash="";
+    const _token=hash.access_token;
+
+    if(_token){
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe()
+      .then(user=>{
+        console.log(user)
+      })
+      .catch(err=> console.log(err))
+    }
+
+    console.log(token);
+  },[]);
+
   return (
     <div className="App">
-    <Login/>
+      {
+        token?(<h1>hello</h1>):(<Login/>)
+      }
+    
     </div>
   );
 }
