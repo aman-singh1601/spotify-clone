0f10ac6baee43d137b7b4ac575d18de104bd09b0
Aman <aman16singh2001@gmail.com>
2023-06-16 19:38:05 +0530

    created the header structure

---
+++ src/Player/Body/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header({spotify}) {
+  return (
+    <div className='header'>
+        <div className='header_left'>
+
+        </div>
+        <div className='header_right'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
